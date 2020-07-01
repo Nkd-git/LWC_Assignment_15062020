e50d80c9cd19d66c66b1b23793fd2327d8875686
Nkd-git <67096170+Nkd-git@users.noreply.github.com>
2020-07-01 20:03:32 +0530

    Add files via upload

--- lwc/lwcPocChild15062020/lwcPocChild15062020.js
+++ lwc/lwcPocChild15062020/lwcPocChild15062020.js
@@ -1,4 +1,4 @@
-import { LightningElement,wire,api,track} from 'lwc';
+import { LightningElement, wire, api, track } from 'lwc';
 import getAccounts from '@salesforce/apex/FetchAccounts.getAccounts'
 
 export default class LwcPocChild15062020 extends LightningElement {
@@ -7,54 +7,50 @@ export default class LwcPocChild15062020 extends LightningElement {
     @track accounts;
     @track error;
     @track filterValue;
-    accountsArray=[];
-    //accountsFiltered=[];
+    accountsArray = [];
     toggleAccounts;
     loadedData;
-    //accountName='Nish';
-    //recdNum=4;
-
-    @wire(getAccounts,{searchAcc : '$accountName',limitRecds : '$recdNum'}) accountsList({ error, data }){
+    // Fetch the accounts from server
+    @wire(getAccounts, { searchAcc: '$accountName', limitRecds: '$recdNum' }) accountsList({ error, data }) {
         if (data) {
             this.accounts = data;
             this.accountsArray.push(...this.accounts);
-            console.log(this.accounts);
+            console.log('WIRED ACCOUNTS ' + this.accounts);
             this.error = undefined;
-            this.loadedData=true;
-         } else if (error) {
+            this.loadedData = true;
+        } else if (error) {
             this.error = error;
             this.accounts = undefined;
-            //console.log(error);
-      }else{
-          this.loadedData = false;
-      }
+        } else {
+            this.loadedData = false;
+        }
     }
-    checkAccount(value){
-       return value == this.filterValue;
-      }
-    handleFilter(event){
+    // Fetch the filter text
+    handleFilter(event) {
         this.filterValue = event.target.value;
     }
-    handleFilterData(){
-        var accountsFiltered=[];
+    // Logic for filter results
+    handleFilterData() {
+        var accountsFiltered = [];
         this.error = undefined;
         let textVal = this.filterValue.replace(/%/g, "");
         this.accountsArray.forEach(entryVal => {
-        if(entryVal.Name.trim().includes(textVal.trim()) && textVal.trim().length>0 ){
-        accountsFiltered.push(entryVal);
-        console.log('Matched Accounts:- '+accountsFiltered);
-    };
-    });
-     if(textVal.trim().length == 0 || this.filterValue==undefined){
-        this.accounts=[...this.accountsArray];
-    }else if(textVal.trim().length > 0 && accountsFiltered != undefined && accountsFiltered != ''){
-        this.accounts=[...accountsFiltered];
-    }
-    else{
-        this.accounts = undefined
-        this.error = 'No accounts found';
-        console.log('Unmatched Accounts');
-    }
+            if (entryVal.Name.trim().includes(textVal.trim()) && textVal.trim().length > 0) {
+                accountsFiltered.push(entryVal);
+                console.log('Matched Accounts:- ' + accountsFiltered);
+            };
+        });
+        if (textVal.trim().length == 0 || this.filterValue == undefined) {
+            this.accounts = [...this.accountsArray];
+        } else if (textVal.trim().length > 0 && accountsFiltered != undefined && accountsFiltered != '') {
+            this.accounts = [...accountsFiltered];
+        }
+        else {
+            this.accounts = undefined
+            this.error = 'No accounts found';
+            console.log('Unmatched Accounts');
+        }
+
     }
 
 }
--- lwc/lwcPocReq15062020/lwcPocReq15062020.js
+++ lwc/lwcPocReq15062020/lwcPocReq15062020.js
@@ -4,24 +4,28 @@ export default class LwcPocReq15062020 extends LightningElement {
     @track accountNm;
     @track recordNumber;
     fetchAccounts;
-    
+
+    // Fetch the Account Name
     handleName(event) {
         this.fetchAccounts = false;
         const accName = event.target.value;
         this.accountNm = accName.trim();
     }
+    // Fetch the number of records to return
+
     handleNumber(event) {
         this.fetchAccounts = false;
         const accNumber = event.target.value;
         this.recordNumber = accNumber.trim();
     }
+    // Check valid inputs
     handleSearch(event) {
-        if ((this.accountNm != undefined && this.accountNm != '') && (this.recordNumber != undefined && this.recordNumber!='' && this.recordNumber!=0)) {
+        if ((this.accountNm != undefined && this.accountNm != '') && (this.recordNumber != undefined && this.recordNumber != '' && this.recordNumber != 0)) {
             this.fetchAccounts = true;
         } else {
             this.fetchAccounts = false;
         }
-        console.log('Account Name: ' + this.accountNm + ' and account number is: ' + this.recordNumber + ' '+ this.fetchAccounts);
+        console.log('Account Name: ' + this.accountNm + ' and account number is: ' + this.recordNumber + ' ' + this.fetchAccounts);
     }
-    
+
 }
